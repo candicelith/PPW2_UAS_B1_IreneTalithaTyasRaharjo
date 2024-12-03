7e4d243b70155412bba1f0394951e94569fa0682
candicelith <irenetalithatyasraharjo@mail.ugm.ac.id>
2024-12-03 13:27:04 +0700

    bugfix#9

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Transaksi;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $transaksi_count =
-        return view('dashboard', );
+        $transaksi_count = Transaksi::count();
+        return view('dashboard', ['transaksi_count' => $transaksi_count]);
     }
 }
