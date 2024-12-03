11fdbc2844f9fdc97d46d7c29c9aff90317b3555
candicelith <irenetalithatyasraharjo@mail.ugm.ac.id>
2024-12-03 13:51:45 +0700

    bugfix#12

--- database/migrations/2024_11_01_005141_create_transaksi_table.php
+++ database/migrations/2024_11_01_005141_create_transaksi_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema:create('transaksi', function (Blueprint $table) {
+        Schema::create('transaksi', function (Blueprint $table) {
             $table->id();
             $table->date('tanggal_pembelian');
             $table->integer('total_harga');
