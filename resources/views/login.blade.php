6ee0251298bade3348a28cbd6001def6b91dbe56
candicelith <irenetalithatyasraharjo@mail.ugm.ac.id>
2024-12-03 14:41:15 +0700

    bugfix#23

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,40 +1,45 @@
+@extends('layouts')
 
-<div class="row justify-content-center mt-5">
-    <div class="col-md-8">
+@section('content')
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
 
-        <div class="card">
-            <div class="card-header">Login</div>
-            <div class="card-body">
-                @if ($message = Session::get('success'))
-                    <div class="alert alert-success">
-                        {{ $message }}
-                    </div>
-                @endif
-                <form action="{{ route('authenticate') }}" method="post">
-                    @csrf
-                    <div class="mb-3 row">
-                        <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email Address</label>
-                        <div class="col-md-6">
-                            <input type="email" class="form-control @error('') is-invalid @enderror" name="" value="{{ old('') }}">
-                            @if ($errors->has(''))
-                                <span class="text-danger">{{ $errors->first('') }}</span>
-                            @endif
+            <div class="card">
+                <div class="card-header">Login</div>
+                <div class="card-body">
+                    @if ($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            {{ $message }}
                         </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="password" class="col-md-4 col-form-label text-md-end text-start">Password</label>
-                        <div class="col-md-6">
-                            <input type="password" class="form-control @error('') is-invalid @enderror" name="">
-                            @if ($errors->has(''))
-                                <span class="text-danger">{{ $errors->first('') }}</span>
-                            @endif
+                    @endif
+                    <form action="{{ route('authenticate') }}" method="post">
+                        @csrf
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email
+                                Address</label>
+                            <div class="col-md-6">
+                                <input type="email" class="form-control @error('') is-invalid @enderror" name=""
+                                    value="{{ old('') }}">
+                                @if ($errors->has(''))
+                                    <span class="text-danger">{{ $errors->first('') }}</span>
+                                @endif
+                            </div>
                         </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Login">
-                    </div>
-                </form>
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end text-start">Password</label>
+                            <div class="col-md-6">
+                                <input type="password" class="form-control @error('') is-invalid @enderror" name="">
+                                @if ($errors->has(''))
+                                    <span class="text-danger">{{ $errors->first('') }}</span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Login">
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+@endsection
